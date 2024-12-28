23f30506e492c0c6816201c6689b06e71d918bf0
dimasrol <yautyati3@gmail.com>
2024-12-28 10:15:59 +0700

    $commitMessage

---
+++ New Windows Batch File.bat
@@ -0,0 +1,11 @@
+cd "C:\Users\dimas\Downloads\proxy-scraper-checker-main\out"
+
+# Pastikan Git terinstal dan tersedia di PATH
+git add .
+
+# Commit perubahan dengan pesan otomatis yang menyertakan waktu
+$commitMessage = "Auto commit - " + (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+git commit -m $commitMessage
+
+# Push ke remote repository (ganti dengan nama branch yang sesuai)
+git push origin master
